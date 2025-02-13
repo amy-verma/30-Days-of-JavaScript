ca62325215f42a6c4112a3138dcea341d4fa86b1
Amit Verma <amitjsr8786@gmail.com>
2025-02-13 12:16:12 +0530

    Day-3

---
+++ index.js
@@ -0,0 +1,13 @@
+// let sidea=parseFloat(prompt("Enter the side a"))
+// let sideb=parseFloat(prompt("Enter side b"))
+// let sidec=parseFloat(prompt("Enter side c"))
+// let perimeter=sidea+sideb+sidec;
+// console.log(perimeter)
+// alert(`${perimeter}`)
+
+let length=parseInt(prompt("Enter length"))
+let width=parseInt(prompt("Enter width"))
+let area=length*width;
+let perimeter=2*length*width;
+console.log(area,perimeter)
+alert(`area:${area},perimeter:${perimeter}`)
